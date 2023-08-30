3c1d1460aac19ec316c93b888780aae5d50da4b3
mohamed271220 <mohamedaprahym@gmail.com>
2023-08-30 14:53:17 +0300

    redux

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,11 +3,57 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
+import cartReducer from "./store/cartSlice.js";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { PersistGate } from "redux-persist/integration/react";
+
+
+const persistConfig = {
+  key: "cart",
+  storage,
+  version: 1,
+};
+
+const rootPersistConfig = {
+  key: "root",
+  storage: storage,
+};
+
+const rootReducer = combineReducers({
+  cart: persistReducer(persistConfig, cartReducer),
+});
+
+const persistedReducer = persistReducer(rootPersistConfig, rootReducer);
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoreActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistStore(store)}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
   </React.StrictMode>
 );
