c59839f6d2a5755a6bb68ee7d4c16418128bdfa3
mohamed271220 <mohamedaprahym@gmail.com>
2023-10-24 19:08:27 +0300

    feat:blog page structure and data model. task:finish the image upload for blog page

--- client/src/scenes/AddPost/index.jsx
+++ client/src/scenes/AddPost/index.jsx
@@ -3,6 +3,7 @@ import * as Yup from "yup";
 import axios from "axios";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import Banner from "../../components/Banner";
 
 const blogPostSchema = Yup.object().shape({
     title: Yup.string().required("Title is required"),
@@ -25,16 +26,6 @@ const initialValues = {
     body: [{ type: "title", content: "" }],
 };
 
-const onSubmit = async (values, { setSubmitting }) => {
-    try {
-        const response = await axios.post("/api/blog-posts", values);
-        console.log(response.data);
-        setSubmitting(false);
-    } catch (error) {
-        console.error(error);
-        setSubmitting(false);
-    }
-};
 
 const BlogPostForm = () => {
     const [addedPhotos, setAddedPhotos] = useState();
@@ -61,7 +52,7 @@ const BlogPostForm = () => {
                 // console.log(filenames);
 
                 setAddedPhotos(filename)
-                
+
             })
             .catch((error) => {
                 console.log(error);
@@ -75,111 +66,177 @@ const BlogPostForm = () => {
         setIsLoading(false);
     }
 
+    const onSubmit = async (values, { setSubmitting }) => {
+        try {
+            const response = await axios.post("/api/blog-posts", values);
+            console.log(response.data);
+            setSubmitting(false);
+        } catch (error) {
+            console.error(error);
+            setSubmitting(false);
+        }
+    };
 
     return (
-        <Formik
-            initialValues={initialValues}
-            validationSchema={blogPostSchema}
-            onSubmit={onSubmit}
-        >
-            {({ values, errors, touched, isSubmitting }) => (
-                <Form>
-                    <label htmlFor="title">Title</label>
-                    <Field type="text" name="title" />
-                    <ErrorMessage name="title" />
+        <div>
+            <Banner title={'Add Post'} path={["Home", "Admin", "Add Post"]} />
+            <Formik
+                initialValues={initialValues}
+                validationSchema={blogPostSchema}
+                onSubmit={onSubmit}
+            >
+                {({ values, errors, touched, isSubmitting }) => (
+                    <Form>
+                        <label htmlFor="title">Title</label>
+                        <Field type="text" name="title" />
+                        <ErrorMessage name="title" />
 
-                    <label htmlFor="author">Author</label>
-                    <Field type="text" name="author" />
-                    <ErrorMessage name="author" />
+                        <label htmlFor="author">Author</label>
+                        <Field type="text" name="author" />
+                        <ErrorMessage name="author" />
 
-                    <label htmlFor="image">Image URL</label>
-                    <Field type="text" name="image" />
-                    <ErrorMessage name="image" />
-
-                    <label htmlFor="tags">Tags</label>
-                    <FieldArray name="tags">
-                        {(arrayHelpers) => (
-                            <div>
-                                {values.tags.map((tag, index) => (
-                                    <div key={index}>
-                                        <Field name={`tags.${index}`} />
+                        <label htmlFor="image">Image URL</label>
+                        <div className="form-control__collection">
+                            <label htmlFor="file" className="label-upload">Upload Images</label>
+                            {addedPhotos &&
+                                <>
+                                    <div className="form-control__uploader">
+                                        <img src={addedPhotos}
+                                            alt="" />
                                         <button
-                                            type="button"
-                                            onClick={() => arrayHelpers.remove(index)}
+                                            onClick={() => removePhoto()}
+                                            className="btn-1 "
                                         >
-                                            -
+                                            <svg
+                                                xmlns="http://www.w3.org/2000/svg"
+                                                fill="none"
+                                                viewBox="0 0 24 24"
+                                                strokeWidth={1.5}
+                                                stroke="currentColor"
+                                            >
+                                                <path
+                                                    strokeLinecap="round"
+                                                    strokeLinejoin="round"
+                                                    d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+                                                />
+                                            </svg>
                                         </button>
+
+
                                     </div>
-                                ))}
-                                <button
-                                    type="button"
-                                    onClick={() => arrayHelpers.push("")}
+                                </>
+
+                            }
+                            <label className="form-control__label">
+                                <input
+                                    type="file"
+                                    name="file"
+                                    readId=""
+                                    className="hidden"
+                                    onChange={uploadPhoto}
+                                />
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    strokeWidth={1.5}
+                                    stroke="currentColor"
                                 >
-                                    +
-                                </button>
-                            </div>
-                        )}
-                    </FieldArray>
+                                    <path
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5"
+                                    />
+                                </svg>
+                                Upload
+                            </label>
+                        </div>
+                
+                        <label htmlFor="tags">Tags</label>
+                        <FieldArray name="tags">
+                            {(arrayHelpers) => (
+                                <div>
+                                    {values.tags.map((tag, index) => (
+                                        <div key={index}>
+                                            <Field name={`tags.${index}`} />
+                                            <button
+                                                type="button"
+                                                onClick={() => arrayHelpers.remove(index)}
+                                            >
+                                                -
+                                            </button>
+                                        </div>
+                                    ))}
+                                    <button
+                                        type="button"
+                                        onClick={() => arrayHelpers.push("")}
+                                    >
+                                        +
+                                    </button>
+                                </div>
+                            )}
+                        </FieldArray>
 
-                    <label htmlFor="body">Body</label>
-                    <FieldArray name="body">
-                        {(arrayHelpers) => (
-                            <div>
-                                {values.body.map((section, index) => (
-                                    <div key={index}>
-                                        <label htmlFor={`body.${index}.type`}>Type</label>
-                                        <Field
-                                            as="select"
-                                            name={`body.${index}.type`}
-                                            validate={(value) => {
-                                                const otherSections = values.body.filter(
-                                                    (s, i) => i !== index
-                                                );
-                                                const hasDuplicate = otherSections.some(
-                                                    (s) => s.type === value
-                                                );
-                                                return hasDuplicate
-                                                    ? "Duplicate section type"
-                                                    : undefined;
-                                            }}
-                                        >
-                                            <option value="title">Title</option>
-                                            <option value="paragraph">Paragraph</option>
-                                            <option value="image">Image</option>
-                                        </Field>
-                                        <ErrorMessage name={`body.${index}.type`} />
+                        <label htmlFor="body">Body</label>
+                        <FieldArray name="body">
+                            {(arrayHelpers) => (
+                                <div>
+                                    {values.body.map((section, index) => (
+                                        <div key={index}>
+                                            <label htmlFor={`body.${index}.type`}>Type</label>
+                                            <Field
+                                                as="select"
+                                                name={`body.${index}.type`}
+                                                validate={(value) => {
+                                                    const otherSections = values.body.filter(
+                                                        (s, i) => i !== index
+                                                    );
+                                                    const hasDuplicate = otherSections.some(
+                                                        (s) => s.type === value
+                                                    );
+                                                    return hasDuplicate
+                                                        ? "Duplicate section type"
+                                                        : undefined;
+                                                }}
+                                            >
+                                                <option value="title">Title</option>
+                                                <option value="paragraph">Paragraph</option>
+                                                <option value="image">Image</option>
+                                            </Field>
+                                            <ErrorMessage name={`body.${index}.type`} />
 
-                                        <label htmlFor={`body.${index}.content`}>Content</label>
-                                        <Field
-                                            as="textarea"
-                                            name={`body.${index}.content`}
-                                        />
-                                        <ErrorMessage name={`body.${index}.content`} />
+                                            <label htmlFor={`body.${index}.content`}>Content</label>
+                                            <Field
+                                                as="textarea"
+                                                name={`body.${index}.content`}
+                                            />
+                                            <ErrorMessage name={`body.${index}.content`} />
 
-                                        <button
-                                            type="button"
-                                            onClick={() => arrayHelpers.remove(index)}
-                                        >
-                                            -
-                                        </button>
-                                    </div>
-                                ))}
-                                <button
-                                    type="button"
-                                    onClick={() => arrayHelpers.push({ type: "title", content: "" })}
-                                >
-                                    +
-                                </button>
-                            </div>
-                        )}
-                    </FieldArray>
+                                            <button
+                                                type="button"
+                                                onClick={() => arrayHelpers.remove(index)}
+                                            >
+                                                -
+                                            </button>
+                                        </div>
+                                    ))}
+                                    <button
+                                        type="button"
+                                        onClick={() => arrayHelpers.push({ type: "title", content: "" })}
+                                    >
+                                        +
+                                    </button>
+                                </div>
+                            )}
+                        </FieldArray>
 
-                    <button type="submit" disabled={isSubmitting}>
-                        Submit
-                    </button>
-                </Form>
-            )}
-        </Formik>
+                        <button type="submit" disabled={isSubmitting}>
+                            Submit
+                        </button>
+                    </Form>
+                )}
+            </Formik>
+        </div>
     );
 };
 export default BlogPostForm
