b601734b30c193419637aabedeb937bbadfe48d2
mohamed271220 <mohamedaprahym@gmail.com>
2024-02-13 14:29:08 +0200

    r

--- server/routes/Auth.js
+++ server/routes/Auth.js
@@ -3,24 +3,21 @@ const passport = require("passport");
 const User = require("../models/User");
 
 router.get("/login/success", async (req, res) => {
-  if (req.user && req.user.id) {
-    try {
-      const data = await User.findOne({ googleId: req.user.id });
-      if (data) {
-        res.status(200).json({
-          error: false,
-          message: "Successfully Logged In",
-          user: req.user,
-          data: data,
-        });
-      } else {
-        res.status(404).json({ error: true, message: "User not found in database" });
-      }
-    } catch (error) {
-      res.status(500).json({ error: true, message: "Database error", details: error.message });
+  try {
+    console.log(req.user);
+    const data = await User.findOne({ googleId: req.user.id });
+    if (data) {
+      res.status(200).json({
+        error: false,
+        message: "Successfully Logged In",
+        user: req.user,
+        data: data,
+      });
+    } else {
+      res.status(404).json({ error: true, message: "User not found in database" });
     }
-  } else {
-    res.status(403).json({ error: true, message: "Not Authorized" });
+  } catch (error) {
+    res.status(500).json({ error: true, message: "Database error", details: error.message });
   }
 });
 
