801339c8e06747a27ea9ec6a51fbfd7026fe4762
mohamed271220 <mohamedaprahym@gmail.com>
2024-02-13 13:23:31 +0200

    r

--- server/routes/shop.js
+++ server/routes/shop.js
@@ -2,13 +2,7 @@
 const express = require("express");
 const productController = require("../controllers/shop");
 const router = express.Router();
-const { resolve } = require("path");
-router.use(express.static(process.env.STATIC_DIR));
 
-router.get("/", (req, res) => {
-    const path = resolve(process.env.STATIC_DIR + "/index.html");
-    res.sendFile(path);
-});
 
 router.get("/products", productController.getProducts);
 router.get("/products/:productId", productController.getProduct);
